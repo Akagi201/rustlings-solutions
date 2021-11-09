4bace23cbd0d49eba01588960acfd691c6db21a4
Akagi201 <akagi201@gmail.com>
2021-11-09 21:20:22 +0800

    solved

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -21,8 +21,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation
 // and return an Ok result of inner type Color.
 // You need to create an implementation for a tuple of three integers,
@@ -32,10 +30,23 @@ enum IntoColorError {
 // but the slice implementation needs to check the slice length!
 // Also note that correct RGB color values must be integers in the 0..=255 range.
 
+fn is_rgb_limited(val: i16) -> bool {
+    (0..=255).contains(&val)
+}
+
 // Tuple implementation
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        if !(is_rgb_limited(tuple.0) && is_rgb_limited(tuple.1) && is_rgb_limited(tuple.2)) {
+            Err(Self::Error::IntConversion)
+        } else {
+            Ok(Color {
+                red: tuple.0 as u8,
+                green: tuple.1 as u8,
+                blue: tuple.2 as u8,
+            })
+        }
     }
 }
 
@@ -43,6 +54,15 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        if !(is_rgb_limited(arr[0]) && is_rgb_limited(arr[1]) && is_rgb_limited(arr[2])) {
+            Err(Self::Error::IntConversion)
+        } else {
+            Ok(Color {
+                red: arr[0] as u8,
+                green: arr[1] as u8,
+                blue: arr[2] as u8,
+            })
+        }
     }
 }
 
@@ -50,6 +70,20 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            Err(Self::Error::BadLen)
+        } else if !(is_rgb_limited(slice[0])
+            && is_rgb_limited(slice[1])
+            && is_rgb_limited(slice[2]))
+        {
+            Err(Self::Error::IntConversion)
+        } else {
+            Ok(Color {
+                red: slice[0] as u8,
+                green: slice[1] as u8,
+                blue: slice[2] as u8,
+            })
+        }
     }
 }
 
